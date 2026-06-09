f1d8cc3b9540612b1e6596e0e316bec94edf4831
Student- Amandeep Varma <amandeepvarma700@gmail.com>
2026-06-09 19:54:52 +0530

    DSA

---
+++ array/longestSumIncludingNegative.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// compute length of the longest subarray with sum 0
+int maxLen(int A[], int n) {
+  
+}
+
+// program entry
+int main() {
+  // sample input
+  int A[] = {9, -3, 3, -1, 6, -5};
+  // compute size
+  int n = sizeof(A) / sizeof(A[0]);
+  // print result
+  cout << maxLen(A, n) << endl;
+  // exit
+  return 0;
+}
